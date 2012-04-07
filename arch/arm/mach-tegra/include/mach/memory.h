dd05374b0c0a025ca5c96b8030b247c3a9c3fac0
Mike Kasick <mike@kasick.org>
2012-04-06 23:36:32 -0400

    Support hard booting to a kexec kernel.
    See KEXEC_HARDBOOT config option help for details.
    
    Conflicts:
    
    	arch/arm/configs/crespo_defconfig

--- arch/arm/mach-tegra/include/mach/memory.h
+++ arch/arm/mach-tegra/include/mach/memory.h
@@ -26,9 +26,15 @@
 #if defined(CONFIG_ARCH_TEGRA_2x_SOC)
 #define PLAT_PHYS_OFFSET		UL(0)
 #define END_MEM				UL(0x40000000)
+#if defined(CONFIG_KEXEC_HARDBOOT)
+#define KEXEC_HB_PAGE_ADDR 		UL(0x3FE00000)
+#endif
 #else
 #define PLAT_PHYS_OFFSET		UL(0x80000000)
 #define END_MEM				UL(0xC0000000)
+#if defined(CONFIG_KEXEC_HARDBOOT)
+#define KEXEC_HB_PAGE_ADDR 		UL(0xBFE00000)
+#endif
 #endif
 
 /*
@@ -39,6 +45,8 @@
 #define NET_IP_ALIGN	0
 
 #define CONSISTENT_DMA_SIZE	(14 * SZ_1M)
+
+
 
 #endif
 
